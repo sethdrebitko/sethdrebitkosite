556ce0197594ebec2ae07959ec982e22fab4e6de
Seth Drebitko <sethdrebitko@gmail.com>
2022-06-21 13:35:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -28,40 +28,25 @@ try {
   // replace el with newEL
   DefaultMenu.outerHTML = 
   `
-  <h1>Want to be Pen Pals?</h1>
-
-      <p>
-        Can't join the Awesome Club, but still want to keep up to date on all the goodness being created?
-        Feel free to join the still awesome Pen Pals newsletter.
-      </p>
-
-      <!-- Begin Mailchimp Signup Form -->
-      <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-      <style type="text/css">
-        #mc_embed_signup {
-          clear: left;
-          font: 14px Helvetica, Arial, sans-serif;
-        }
-
-        /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-                                    We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-      </style>
-      <div id="mc_embed_signup">
-        <form
-          action="https://sethhimself.us5.list-manage.com/subscribe/post?u=2e15384af576d3ddd1e9b70ce&amp;id=31adad69fc"
-          method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank"
-          novalidate>
-          <div id="mc_embed_signup_scroll">
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text"
-                name="b_2e15384af576d3ddd1e9b70ce_31adad69fc" tabindex="-1" value=""></div>
-            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
-                class="button"></div>
-          </div>
-        </form>
-      </div>
-
-      <!--End mc_embed_signup-->
+  <div id="revue-embed">
+  <form action="https://www.getrevue.co/profile/sethdrebitko/add_subscriber" method="post" id="revue-form" name="revue-form"  target="_blank">
+  <div class="revue-form-group">
+    <label for="member_email">Email address</label>
+    <input class="revue-form-field" placeholder="Your email address..." type="email" name="member[email]" id="member_email">
+  </div>
+  <div class="revue-form-group">
+    <label for="member_first_name">First name <span class="optional">(Optional)</span></label>
+    <input class="revue-form-field" placeholder="First name... (Optional)" type="text" name="member[first_name]" id="member_first_name">
+  </div>
+  <div class="revue-form-group">
+    <label for="member_last_name">Last name <span class="optional">(Optional)</span></label>
+    <input class="revue-form-field" placeholder="Last name... (Optional)" type="text" name="member[last_name]" id="member_last_name">
+  </div>
+  <div class="revue-form-actions">
+    <input type="submit" value="Subscribe" name="member[subscribe]" id="member_submit">
+  </div>
+  <div class="revue-form-footer">By subscribing, you agree with Revue’s <a target="_blank" href="https://www.getrevue.co/terms">Terms of Service</a> and <a target="_blank" href="https://www.getrevue.co/privacy">Privacy Policy</a>.</div>
+  </form>
+</div>
   `;
 } catch{}
